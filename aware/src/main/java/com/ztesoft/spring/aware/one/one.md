106c747a93c50c88a87461045f6cb3d96dab71c5
田陆野 <tian.luye@zte.com.cn>
2017-08-05 14:08:24 +0800

    bean 生命周期

--- aware/src/main/java/com/ztesoft/spring/aware/one/one.md
+++ aware/src/main/java/com/ztesoft/spring/aware/one/one.md
@@ -2,3 +2,8 @@
 - 初步实现了 Aware接口，与 Spring耦合，获取到当前应用的上下文
 - 根据应用上下文读取 设置的环境变量值
 - 应用上下文派发事件(extends ApplicationEvent)，监听事件(implements ApplicationListener<? extends ApplicationEvent>)
+
+```
+-Dspring.config.location="E:\spring-study-test\ZSMART_HOME\user.properties"
+ztesoft=TestZteSoft
+```
--- lifecycle/src/main/java/com/ztesoft/spring/lifecycle/one/LifeCycleMain.java
+++ lifecycle/src/main/java/com/ztesoft/spring/lifecycle/one/LifeCycleMain.java
@@ -1,12 +1,18 @@
 package com.ztesoft.spring.lifecycle.one;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author tian.lue
  */
 public class LifeCycleMain {
 
     public static void main(String[] args) {
-
+        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-lifecycle.xml");
+        Person person = applicationContext.getBean(Person.class);
+        System.out.println(person);
+        applicationContext.close();
     }
 
 }
